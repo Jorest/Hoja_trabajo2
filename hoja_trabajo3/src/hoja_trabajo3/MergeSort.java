d7bec4074f4cd339f4dc25e2e05e20a70ccc3d24
Jorge <Jorge@192.168.1.2>
2014-07-28 17:50:12 -0600

    primera parte del merge

--- hoja_trabajo3/src/hoja_trabajo3/MergeSort.java
+++ hoja_trabajo3/src/hoja_trabajo3/MergeSort.java
@@ -6,10 +6,64 @@
 
 package hoja_trabajo3;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 /**
  *
  * @author Irene
  */
 public class MergeSort {
+
+//area de datos
+  ArrayList<Integer> dato1 = new ArrayList<Integer>(200);
+  ArrayList<Integer> dato2 = new ArrayList<Integer>(200);
+  ArrayList<Integer> dato3 = new ArrayList<Integer>(200);
+   
+    //funcion que mezcla 2 cadeas y las ordena
+    //entradas 2 ArrayList
+    //Salida:Cadena ordenada
     
+    public ArrayList<Integer> Merge(ArrayList<Integer> data1,ArrayList<Integer> data2) 
+
+    {
+       ArrayList<Integer> data3 = new ArrayList<Integer>(data1.size()+data2.size());
+       Integer a;
+       Integer b;
+       Integer cont=0;
+       Integer conta=0;
+       Integer contb=0;
+ 
+       
+       for(int i=0;i==data3.size();i++)
+       {
+           a=data1.get(conta);
+           b=data2.get(contb);
+           if (a<=b){
+            data3.set(cont,a);
+            conta++;
+                    }
+           else{
+            data3.set(cont,a);
+            contb++;
+               }
+           cont++;
+       }
+     return data3;   
+    }
+        
+    public void mergeSort(ArrayList<Integer> data1){
+        
+        dato3=Merge(dato1,dato2);
+    }
 }
+
+
+
+
+
+
+
+
+
+
